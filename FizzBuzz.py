4dab5a8faff38a0e840ec8d31bd32c0db28629c3
Andrea <andrea.vazquez@pinteam.eu>
2019-05-05 15:00:52 +0200

    Changed buzz for buzz branch

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -9,7 +9,7 @@ for num in range(1, 21):
     if num % 3 == 0:
         string = string + "Fizz"
     if num % 5 == 0:
-        string = string + "Buzz"
+        string = string + "BuzzBranch"
     if num % 5 != 0 and num % 3 != 0:
         string = string + str(num)
     print(string)
